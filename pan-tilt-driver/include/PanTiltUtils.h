22e173870ddb9fd10058ae803832cd979a192333
Lyang Medeiros <lyang.medeiros@ee.ufcg.edu.br>
2022-05-11 19:50:50 -0300

    :sparkles: feat: implements the suppot control by serial

--- pan-tilt-driver/include/PanTiltUtils.h
+++ pan-tilt-driver/include/PanTiltUtils.h
@@ -1,7 +1,57 @@
 
-#ifndef UTILS_H
-#define UTILS_H
+#ifndef PAN_TILT_UTILS_H
+#define PAN_TILT_UTILS_H
+
+#define DEBUG 1
 
 #define SERIAL_BAUD_RATE  115200
+#define SERIAL_TIMEOUT    200
+#define SERIAL_TERMINATOR '!'
+#define SERIAL_SEPARATOR '/'
+#define VALID_MSG_LENGTH 11
+
+#define WAIT_FOR_SERIAL() while(!Serial){}
+
+#ifndef FORMAT
+#define FORMAT(data) (String(data).c_str())
+#endif
+
+#define MASK_STR "%*"
+#define MASK_LEN 2
+
+String _REPLACE_FIRST_MASK_(String str, String arg);
+
+void DEBUG_MSG (String msg, ...)
+{
+    String str, out;
+    va_list arguments;                     
+    
+    if (DEBUG)
+    {
+        va_start (arguments, msg);
+        out = msg;
+        
+        do
+        {
+            str = va_arg(arguments, char*);
+            if (str == msg) continue;
+            out = _REPLACE_FIRST_MASK_(out, str);
+        } while (str != msg);
+
+        va_end (arguments);
+
+        Serial.println(String("DEBUG MESSAGE: " + String(out)));
+        Serial.flush();
+    }
+}
+
+String _REPLACE_FIRST_MASK_(String str, String arg)
+{
+    String _str = str.substring(str.indexOf(MASK_STR) + MASK_LEN, str.length());
+    str.remove(str.indexOf(MASK_STR) + MASK_LEN, str.length() - (str.indexOf(MASK_STR) + MASK_LEN));
+    str.replace(MASK_STR, arg);
+    str.concat(_str);
+    return str;
+}
 
 #endif
