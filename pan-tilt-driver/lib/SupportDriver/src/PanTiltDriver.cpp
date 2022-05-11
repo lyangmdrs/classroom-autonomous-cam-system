27b4cc4b8433da3ec257a1ff84f61d9fe2bf08a4
Lyang Medeiros <lyang.medeiros@ee.ufcg.edu.br>
2022-05-11 19:45:27 -0300

    :sparkles: Implements the PanTiltDriver Class

---
+++ pan-tilt-driver/lib/SupportDriver/src/PanTiltDriver.cpp
@@ -0,0 +1,53 @@
+#include <Arduino.h>
+#include "PanTiltDriver.h"
+
+PanTiltDriver::PanTiltDriver()
+{
+    panTimeOn = PAN_MIDDLE;
+    tiltTimeOn = TILT_MIDDLE;
+}
+
+void PanTiltDriver::configTimersAndRegisters()
+{
+  TCCR2A = 0xA3; // Sets PWM
+  TCCR2B = 0x04; // Sets Prescaler 
+  TIMSK2 = 0x01; // Sets Timer2 interruption
+  sei(); // Enables global interruption
+}
+
+void PanTiltDriver::begin()
+{
+    pinMode(PAN_MOTOR_PIN, OUTPUT);
+    pinMode(TILT_MOTOR_PIN, OUTPUT);
+    configTimersAndRegisters();
+}
+
+void PanTiltDriver::interruptionHandler()
+{
+    // Not Implemented
+}
+
+void PanTiltDriver::setPanTimeOn(int value)
+{
+    panTimeOn = SAFE_PAN_TON(value);
+}
+void PanTiltDriver::setTiltTimeOn(int value)
+{
+    tiltTimeOn = SAFE_TILT_TON(value);
+}
+void PanTiltDriver::incrementPanTimeOn(int step)
+{
+    panTimeOn = SAFE_PAN_TON(panTimeOn + step);
+}
+void PanTiltDriver::incrementTiltTimeOn(int step)
+{
+    tiltTimeOn = SAFE_TILT_TON(tiltTimeOn + step);
+}
+int PanTiltDriver::getPanTimeOn()
+{
+    return panTimeOn;
+}
+int PanTiltDriver::getTiltTimeOn()
+{
+    return tiltTimeOn;
+}
---
+++ pan-tilt-driver/lib/SupportDriver/src/PanTiltDriver.h
@@ -0,0 +1,51 @@
+#ifndef PAN_TILT_DRIVER_H
+#define PAN_TILT_DRIVER_H
+
+constexpr float SERVO_PERIOD = 0.02;
+
+constexpr int PAN_MOTOR_PIN = 9;
+constexpr int TILT_MOTOR_PIN = 10;
+
+constexpr int PAN_LIMIT_MIN = 600;
+constexpr int PAN_MIDDLE = 1450;
+constexpr int PAN_LIMIT_MAX = 2300;
+
+constexpr int SAFE_PAN_TON(int t)
+{
+    return (t < PAN_LIMIT_MIN)? PAN_LIMIT_MIN : (t > PAN_LIMIT_MAX)? PAN_LIMIT_MAX : t;
+} 
+
+constexpr int TILT_LIMIT_MIN = 700;
+constexpr int TILT_MIDDLE = 1500;
+constexpr int TILT_LIMIT_MAX = 1800;
+
+constexpr int SAFE_TILT_TON(int t)
+{
+    return (t < TILT_LIMIT_MIN)? TILT_LIMIT_MIN : (t > TILT_LIMIT_MAX)? TILT_LIMIT_MAX : t;
+}
+
+constexpr int CONVERT_SECOND_TO_MILLISECOND(float s)
+{
+    return int(s * 1000);
+}
+
+class PanTiltDriver
+{
+private:
+    int panTimeOn;
+    int tiltTimeOn;
+
+    void configTimersAndRegisters();
+    void interruptionHandler();
+public:
+    PanTiltDriver();
+    void begin();
+    void setPanTimeOn(int value);
+    void setTiltTimeOn(int value);
+    void incrementPanTimeOn(int step);
+    void incrementTiltTimeOn(int step);
+    int getPanTimeOn();
+    int getTiltTimeOn();
+};
+
+#endif
