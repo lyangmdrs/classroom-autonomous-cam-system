199b39955d8e9af6367b42ab5203022642be5b7a
Lyang Medeiros <lyang.mdrs@gmail.com>
2022-06-30 22:20:05 -0300

    :construction: chore: Imporves fps with face detection

--- camera-system/main.py
+++ camera-system/main.py
@@ -1,3 +1,4 @@
+from unittest import result
 import cv2
 import mediapipe as mp
 import numpy as np
@@ -10,45 +11,52 @@ HD_WIDTH = 1280
 HD_HEIGHT = 720
 DRAW_RULES = True
 
-videoInput = cv2.VideoCapture(CAMERA_INDEX)
-videoInput.set(cv2.CAP_PROP_FRAME_WIDTH, HD_WIDTH)
-videoInput.set(cv2.CAP_PROP_FRAME_HEIGHT, HD_HEIGHT)
+mp_face_detection = mp.solutions.face_detection
 mp_drawing = mp.solutions.drawing_utils
 
-def detect_face(frame):
-    mp_face_detection = mp.solutions.face_detection
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, HD_WIDTH)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HD_HEIGHT)
 
-    with mp_face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.95) as face_detection:
-        frame.flags.writeable = False
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = face_detection.process(frame)
-        return results.detections
+def detect_face():
+    pass
 
 
 def main():
-
-    while videoInput.isOpened():
+    with mp_face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.95) as face_detection:
+        while cap.isOpened():
         
-        start = time.time()
+            start = time.time()
+
+            success, image = cap.read()
+            if not success:
+                print("Ignoring empty camera frame.")
+                # If loading a video, use 'break' instead of 'continue'.
+                continue
+
+            # To improve performance, optionally mark the image as not writeable to
+            # pass by reference.
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = face_detection.process(image)
+
+            # Draw the face detection annotations on the image.
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            if results.detections:
+                for detection in results.detections:
+                    mp_drawing.draw_detection(image, detection)
         
-        success, image = videoInput.read()
-
-        detections = detect_face(image)
-        for detection in detections:
-            mp_drawing.draw_detection(image, detection)
-
-        end = time.time()
-        totalTime = end - start
-        fps = 1 / totalTime
-        cv2.putText(image, f'FPS: {int(fps)}', (10,80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,100,255), 2)
-
-        cv2.imshow("Viewer - Press 'ESC' to close", image)
+            end = time.time()
+            totalTime = end - start
+            fps = 1 / totalTime
+            cv2.putText(image, f'FPS: {int(fps)}', (10,80), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,100,255), 2)
         
-        if (cv2.waitKey(5) & 0xFF == 27): # Press ESC
-            
-            videoInput.release()
-            cv2.destroyAllWindows()
-            break
+            # Flip the image horizontally for a selfie-view display.
+            cv2.imshow('MediaPipe Face Detection', image)
+            if cv2.waitKey(5) & 0xFF == 27:
+                break
+    cap.release()
 
 if __name__ == "__main__":
     main()
