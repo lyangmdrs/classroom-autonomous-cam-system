b864803ab8dd2483abc6243da656a8119d80bad0
Lyang Medeiros <lyang.mdrs@gmail.com>
2022-07-08 14:53:37 -0300

    :construction: chore: Implements basic head pose estimation

--- camera-system/main.py
+++ camera-system/main.py
@@ -1,7 +1,7 @@
-from ctypes import sizeof
 import multiprocessing
 from multiprocessing import Process, Queue
 import PIL.Image, PIL.ImageTk
+import mediapipe as mp
 import tkinter as tk
 import numpy as np
 import time
@@ -115,14 +115,102 @@ class FrameAcquisition:
 
 class FrameProcessing:
 
+    mp_holistic = mp.solutions.holistic
+    mp_drawing = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
+
     def __init__(self):
         pass
 
     def head_pose_estimation(self, queue_input, queue_output):
-        while True:
-            input_frame = queue_input.get()
-            output_frame = cv2.cvtColor(input_frame, cv2.COLOR_RGB2GRAY)
-            queue_output.put(output_frame)
+        with self.mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+            while True:
+                input_frame = queue_input.get()
+                # input_frame.flags.writeable = False
+                # frame = cv2.resize(frame, (320, 240), interpolation = cv2.INTER_AREA)
+                results = holistic.process(input_frame)
+                # input_frame.flags.writeable = True
+                # frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+                
+                img_h, img_w, img_c = input_frame.shape
+                face_3d = []
+                face_2d = []
+
+                if results.face_landmarks:
+                    # print(len(results.face_landmarks.landmark))
+                    for idx, lm in enumerate(results.face_landmarks.landmark):
+                        if idx == 33 or idx == 263 or idx == 1 or idx == 61 or idx == 291 or idx == 199:
+                            if idx == 1:
+                                nose_2d = (lm.x * img_w, lm.y * img_h)
+                                nose_3d = (lm.x * img_w, lm.y * img_h, lm.z * 3000)
+                            
+                            x, y = int(lm.x * img_w), int(lm.y * img_h)
+
+                            # Get the 2D Coordinates
+                            face_2d.append([x, y])
+
+                            # Get the 3D Coordinates
+                            face_3d.append([x, y, lm.z])       
+                    
+                    # Convert it to the NumPy array
+                    face_2d = np.array(face_2d, dtype=np.float64)
+
+                    # Convert it to the NumPy array
+                    face_3d = np.array(face_3d, dtype=np.float64)
+
+                    # The camera matrix
+                    focal_length = 1 * img_w
+
+                    cam_matrix = np.array([ [focal_length, 0, img_h / 2],
+                                            [0, focal_length, img_w / 2],
+                                            [0, 0, 1]])
+
+                    # The distortion parameters
+                    dist_matrix = np.zeros((4, 1), dtype=np.float64)
+
+                    # Solve PnP
+                    success, rot_vec, trans_vec = cv2.solvePnP(face_3d, face_2d, cam_matrix, dist_matrix)
+
+                    # Get rotational matrix
+                    rmat, jac = cv2.Rodrigues(rot_vec)
+
+                    # Get angles
+                    angles, mtxR, mtxQ, Qx, Qy, Qz = cv2.RQDecomp3x3(rmat)
+
+                    # Get the y rotation degree
+                    x = angles[0] * 360
+                    y = angles[1] * 360
+                    z = angles[2] * 360
+                
+
+                    # See where the user's head tilting
+                    # if y < -10:
+                    #     text = "Looking Left"
+                    # elif y > 10:
+                    #     text = "Looking Right"
+                    # elif x < -10:
+                    #     text = "Looking Down"
+                    # elif x > 10:
+                    #     text = "Looking Up"
+                    # else:
+                    #     text = "Forward"
+
+                    # Display the nose direction
+                    nose_3d_projection, jacobian = cv2.projectPoints(nose_3d, rot_vec, trans_vec, cam_matrix, dist_matrix)
+
+                    p1 = (int(nose_2d[0]), int(nose_2d[1]))
+                    p2 = (int(nose_2d[0] + y * 10) , int(nose_2d[1] - x * 10))
+                    
+                    cv2.line(input_frame, p1, p2, (255, 0, 0), 3)
+
+                self.mp_drawing.draw_landmarks(input_frame,
+                                               results.face_landmarks,
+                                               self.mp_holistic.FACEMESH_TESSELATION,
+                                               landmark_drawing_spec=None,
+                                               connection_drawing_spec=self.mp_drawing_styles
+                                               .get_default_face_mesh_tesselation_style())
+                queue_output.put(input_frame)
+
 
     def hand_gesture_recognition(self, queue_input, queue_output):
         while True:
@@ -130,7 +218,6 @@ class FrameProcessing:
             output_frame = cv2.cvtColor(input_frame, cv2.COLOR_RGB2BGR)
             queue_output.put(output_frame)
             
-
 
 class FrameServer:
 
@@ -177,12 +264,16 @@ def acquirer(queue):
             continue
 
 def frame_server(input_queue, output_queue, head_pose_queue, hand_gesture_queue):
+    frame_stepping = 5
+    frame_counter = 0
     while True:
         frame = input_queue.get()
         try:
             output_queue.put_nowait(frame)
-            head_pose_queue.put_nowait(frame)
-            hand_gesture_queue.put_nowait(frame)
+            frame_counter = (frame_counter + 1) % frame_stepping
+            if frame_counter == 0:
+                head_pose_queue.put_nowait(frame)
+                hand_gesture_queue.put_nowait(frame)
         except:
             continue
 
@@ -208,7 +299,6 @@ if __name__ == "__main__":
     hand_recognition_process.start()
 
     gui = GuiApplication(pm.queue_raw_frame_server_output, pm.queue_head_pose_estimation_output, pm.queue_hand_gesture_recognition_output)
-    print("fecha queue")
 
     pm.close_all_queues()
 
